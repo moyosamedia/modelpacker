b5f4509389011ec163970161c375cc6243997c2d
Vincent Huisman <vincent@moyosa.nl>
2018-07-23 17:12:02 +0200

    Added adding removing and clearing in the FileList

--- CustomControls/FileList.xaml.cs
+++ CustomControls/FileList.xaml.cs
@@ -1,4 +1,7 @@
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
+using Microsoft.Win32;
 
 namespace ModelPacker.CustomControls
 {
@@ -22,6 +25,71 @@ namespace ModelPacker.CustomControls
         public FileList()
         {
             InitializeComponent();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            string lastAddedFile = FilesList.Items.Count > 0
+                ? FilesList.Items[FilesList.Items.Count - 1].ToString()
+                : null;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                InitialDirectory = !string.IsNullOrEmpty(lastAddedFile)
+                    ? new FileInfo(lastAddedFile).DirectoryName
+                    : System.Reflection.Assembly.GetEntryAssembly().Location,
+                RestoreDirectory = true
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string fileName in openFileDialog.FileNames)
+                {
+                    Add(fileName);
+                }
+            }
+        }
+
+        public void Add(string file)
+        {
+            if (!ContainsFile(file))
+            {
+                FilesList.Items.Add(file);
+            }
+        }
+
+        private void RemoveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (FilesList.SelectedIndex > -1)
+            {
+                while (FilesList.SelectedItems.Count > 0)
+                {
+                    FilesList.Items.RemoveAt(FilesList.SelectedIndex);
+                }
+            }
+        }
+
+        private void ClearButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            FilesList.Items.Clear();
+        }
+
+        public bool ContainsFile(string file)
+        {
+            if (string.IsNullOrEmpty(file))
+                return false;
+
+            foreach (object item in FilesList.Items)
+            {
+                if (item as string == file)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
