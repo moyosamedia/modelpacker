953245917a23968c8785328386e2e685f6735895
Vincent Huisman <vincent@moyosa.nl>
2018-08-01 16:20:52 +0200

    Added the processing of the models

--- ModelPacker.Processor/Processor.cs
+++ ModelPacker.Processor/Processor.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Assimp;
 using Assimp.Unmanaged;
 using ImageMagick;
 using ModelPacker.BinPacker;
@@ -16,11 +19,20 @@ namespace ModelPacker.Processor
             Log.Line(LogType.Debug, "Creating output directory {0}", info.outputDir);
             Directory.CreateDirectory(info.outputDir);
 
-            if (!PackImages(info, out BinPacking imagePacker) || imagePacker.blocks == null || imagePacker.blocks.Length == 0)
+            if (!PackImages(info, out BinPacking imagePacker) || imagePacker.blocks == null ||
+                imagePacker.blocks.Length == 0)
             {
+                Log.Line(LogType.Error, "Failed to process the textures");
                 return false;
             }
 
+            if (!PackModels(info, imagePacker))
+            {
+                Log.Line(LogType.Error, "Failed to process the models");
+                return false;
+            }
+
+            Log.Line(LogType.Info, "Finished processing files");
 
             return false;
         }
@@ -104,6 +116,75 @@ namespace ModelPacker.Processor
             return true;
         }
 
+        private static bool PackModels(ProcessorInfo info, BinPacking imagePacker)
+        {
+            using (AssimpContext importer = new AssimpContext())
+            {
+                ExportFormatDescription[] exportFormatDescriptions = importer.GetSupportedExportFormats();
+
+                Log.Line(LogType.Info, "Shifting uv(w) positions of {0} models to their new location",
+                    info.models.Length);
+
+                for (int i = 0; i < info.models.Length; i++)
+                {
+                    string modelPath = info.models[i];
+                    Scene model = importer.ImportFile(modelPath);
+                    if (!model.HasMeshes)
+                    {
+                        Log.Line(LogType.Warning, "There are no meshes in model file '{0}'", modelPath);
+                        continue;
+                    }
+
+                    Log.Line(LogType.Info, "Processing {0} meshes from model file '{1}'", model.MeshCount, modelPath);
+                    foreach (Mesh mesh in model.Meshes)
+                    {
+                        Log.Line(LogType.Debug, "Found {0} uv(w) channels in mesh '{1}'",
+                            mesh.TextureCoordinateChannelCount, mesh.Name);
+
+                        // TODO: Add support for offsetting uvs per mesh instead of per model file
+                        // Thus having 1 texture per mesh instead of having 1 texture per model file
+                        float scaleX = imagePacker.blocks[i].w / (float) imagePacker.root.w;
+                        float scaleY = imagePacker.blocks[i].h / (float) imagePacker.root.h;
+                        float offsetX = imagePacker.blocks[i].fit.x / (float) imagePacker.root.w;
+                        float offsetY = imagePacker.blocks[i].fit.y / (float) imagePacker.root.h;
+
+                        Log.Line(LogType.Debug, "Calculated scaling multipliers: x: {0}; y: {1}", scaleX, scaleY);
+                        Log.Line(LogType.Debug, "Calculated offsets: x: {0}; y: {1}", offsetX, offsetY);
+
+                        for (int uvwChannel = 0; uvwChannel < mesh.TextureCoordinateChannelCount; uvwChannel++)
+                        {
+                            List<Vector3D> uvw = mesh.TextureCoordinateChannels[uvwChannel];
+                            if (uvw == null || uvw.Count == 0)
+                                continue;
+
+                            for (int n = 0; n < uvw.Count; n++)
+                            {
+                                uvw[n] = new Vector3D(
+                                    uvw[n].X * scaleX + offsetX,
+                                    uvw[n].Y * scaleY + offsetY,
+                                    uvw[n].Z);
+                            }
+
+                            mesh.TextureCoordinateChannels[uvwChannel] = uvw;
+                        }
+                    }
+
+                    ExportFormatDescription exportformat = exportFormatDescriptions.First(x =>
+                        x.FormatId == info.modelExportFormatId);
+
+                    string savePath = Path.Combine(info.outputDir,
+                        string.Format("{0}-{1}.{2}",
+                            info.outputFilesPrefix,
+                            Path.GetFileNameWithoutExtension(modelPath),
+                            exportformat.FileExtension));
+
+                    Log.Line(LogType.Info, "Saving edited model to '{0}'", savePath);
+                    importer.ExportFile(model, savePath, info.modelExportFormatId);
+                }
+            }
+
+            return true;
+        }
 
         private static bool CheckProcessorInfo(ProcessorInfo info)
         {
